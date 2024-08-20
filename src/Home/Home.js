284206a0457c26bcd9db7731e3475fa35c3f6271
lukechar <charbonneau.luke@gmail.com>
2024-08-20 08:40:51 -0600

    Change subtitle on home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,7 +7,7 @@ const Home = (props) => {
                 <div className="row">
                     <div className="col s12">
                         <h2>Luke Charbonneau</h2>
-                        <h4>Software Engineer | Product Developer</h4>
+                        <h4>Data Engineer | Back-end Web Developer</h4>
                         <p className="center-align">
                             Data engineer and back‑end web developer with professional experience that includes building data processing pipelines for NASA missions and
                             leading the development of scientific software products from idea to market. I enjoy writing self‑explanatory code to achieve meaningful technical
